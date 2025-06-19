9561db9097dd5ef19bf69cccf9e87e397e0f8e37
BakaRickyClariS <ricky860526@gmail.com>
2025-06-19 13:56:56 +0800

    首頁元件化、優化 拆分productItem元件出來

---
+++ src/components/ProductItem.js
@@ -0,0 +1,69 @@
+class ProductItem extends HTMLElement {
+  constructor() {
+    super();
+    this.itemInfo = JSON.parse(this.getAttribute("itemInfo") || "[]");
+  }
+  connectedCallback() {
+    this.render();
+  }
+  render() {
+    this.innerHTML = `
+          <ul class="flex-colBox md:flex-row gap-5 px-10 pb-10 md:pb-20">
+        ${this.itemInfo
+          .map(
+            (item) => `
+          <li class="flex-colBox text-center text-[#3F5D45] md:w-1/3">
+            <div class="relative w-full">
+              <img
+                class="w-full aspect-square"
+                src="${item.img}"
+                alt=""
+              />
+              <div class="absolute top-0 left-8 p-5 bg-[#3F5D45] text-white">
+                <h1
+                  class="vlText font-bold text-2xl"
+                >
+                  本日精選
+                </h1>
+              </div>
+              <div class="absolute top-5 right-8">
+                <button>
+                  <img
+                    class="w-10"
+                    src="src/assets/images/favorite_border.png"
+                    alt="favorite"
+                  />
+                </button>
+              </div>
+            </div>
+            <div
+              class="flex-colBox divide-y-1 divide-gray-200 border-1 border-gray-200"
+            >
+              <div
+                class="flex-rowBox divide-x-1 divide-gray-200 text-3xl md:text-2xl items-center"
+              >
+                <h1 class="p-5 w-3/5">${item.name}</h1>
+                <h1 class="p-5 w-2/5 font-bold">NT$ ${item.price}</h1>
+              </div>
+              <div class="flex w-full text-4xl font-bold">
+                <button class="py-8 bg-[#EAF0ED] flex w-full justify-center">
+                  加入購物車
+                </button>
+              </div>
+            </div>
+          </li>
+          `
+          )
+          .join("")}
+        </ul>
+    `;
+  }
+  update(itemInfo) {
+    this.itemInfo = itemInfo;
+    this.render();
+  }
+}
+
+customElements.define("product-item", ProductItem);
+
+export default ProductItem;
