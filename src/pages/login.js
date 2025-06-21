b8cc6df9aca8513aad69d6da09c0da37d586ad7d
BakaRickyClariS <ricky860526@gmail.com>
2025-06-21 21:24:35 +0800

    feat : finish login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,12 +2,12 @@ class Login extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `
-    <ul class="grid grid-cols-1 md:grid-cols-2 items-center max-w-content text-center">
+    <ul class="grid grid-cols-1 md:grid-cols-2 items-center max-w-screen-lg mx-auto text-center py-15">
       <li class="flex-colBox text-[#8DA291] flex-boxCenter bg-[#3F5D45]">
         <form class="space-y-6 w-full">
             <fieldset class="space-y-6 text-2xl p-10">
-              <legend class="text-5xl font-bold block  w-full">會員登入</legend>
-              <div class="flex-rowBox bg-[#EAF0ED] p-10">
+              <h1 class="text-5xl text-white font-bold block w-full py-5">會員登入</h1>
+              <div class="flex-rowBox bg-[#EAF0ED] p-5 items-center gap-5">
                 <label
                   for="email"
                   class="block"
@@ -17,12 +17,12 @@ class Login extends HTMLElement {
                   type="email"
                   id="email"
                   name="email"
-                  class="form-input w-full"
+                  class="w-full"
                   placeholder="電子信箱/手機號碼"
                   required
                 />
               </div>
-              <div class="flex-rowBox bg-[#EAF0ED] p-10">
+              <div class="flex-rowBox bg-[#EAF0ED] p-5 items-center gap-5">
                 <label
                   for="password"
                   class="block"
@@ -32,7 +32,7 @@ class Login extends HTMLElement {
                   type="text"
                   id="password"
                   name="password"
-                  class="form-input w-full"
+                  class="w-full"
                   placeholder="請輸入使用者密碼"
                   required
                 />
@@ -43,27 +43,27 @@ class Login extends HTMLElement {
                   type="checkbox"
                   id="checkbox"
                   name="checkbox"
-                  class="form-checkbox h-5 w-5 text-blue-600 rounded"
+                  class="h-5 w-5"
                   required
                 />
-                <label for="checkbox" class="text-sm text-gray-600">
-                  我同意隱私權政策，並同意依隱私權政策中所述的方式處理自己的資料。
+                <label for="checkbox" class="text-white">
+                  記住我
                 </label>
               </div>
             </fieldset>
             <button
               type="submit"
-              class="w-full bg-[#FFE180] hover:bg-blue-700 text-[#3F5D45] font-medium py-3 px-6 transition-colors"
+              class="w-full bg-[#FFE180] hover:bg-[#EAF0ED] text-[#3F5D45] hover:text-white text-2xl font-bold py-3 px-6 transition-colors"
             >
-              確認送出
+              登入帳號
             </button>
           </form>
       </li>
-      <li class="flex-colBox text-[#8DA291] flex-boxCenter bg-[#EAF0ED]">
-        <h1 class="hidden md:block text-3xl">—— 連結社群帳號 ——</h1>
-        <img src="src/assets/images/ic-facebook-logotype@2x.png" alt="" />
-        <img src="src/assets/images/ic-google@2x.png" alt="" />
-        <img src="src/assets/images/ic-yahoo@2x.png" alt="" />
+      <li class="flex-colBox text-[#8DA291] flex-boxCenter bg-[#EAF0ED] p-10 gap-5">
+        <h1 class="hidden md:block text-3xl p-5">—— 連結社群帳號 ——</h1>
+        <button class="p-5 flex flex-boxCenter bg-white w-full hover:invert"><img class="max-w-[150px]" src="src/assets/images/ic-facebook-logotype@2x.png" alt="facebook" /></button>
+        <button class="p-5 flex flex-boxCenter bg-white w-full hover:invert"><img class="max-w-[150px]" src="src/assets/images/ic-google@2x.png" alt="google" /></button>
+        <button class="p-5 flex flex-boxCenter bg-white w-full hover:invert"><img class="max-w-[150px]" src="src/assets/images/ic-yahoo@2x.png" alt="yahoo" /></button>
       </li>
       
     </ul>
